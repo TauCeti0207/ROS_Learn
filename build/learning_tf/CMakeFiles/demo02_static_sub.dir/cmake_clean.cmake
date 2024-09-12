d5c702a3f271eba2175b6008787e4f31d274250c
TauCeti0207 <2076188013@qq.com>
2024-09-12 17:19:34 +0800

    Auto commit for 2024-09-12

---
+++ build/learning_tf/CMakeFiles/demo02_static_sub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/yzq0207/catkin_ws/devel/lib/learning_tf/demo02_static_sub"
+  "/home/yzq0207/catkin_ws/devel/lib/learning_tf/demo02_static_sub.pdb"
+  "CMakeFiles/demo02_static_sub.dir/src/demo02_static_sub.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo02_static_sub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/learning_tf/CMakeFiles/demo_static_pub.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/yzq0207/catkin_ws/devel/lib/learning_tf/demo_static_pub"
+  "/home/yzq0207/catkin_ws/devel/lib/learning_tf/demo_static_pub.pdb"
+  "CMakeFiles/demo_static_pub.dir/src/demo_static_pub.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo_static_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
