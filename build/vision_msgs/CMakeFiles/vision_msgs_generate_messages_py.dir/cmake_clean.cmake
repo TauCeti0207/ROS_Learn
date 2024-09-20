bf30f848ace134eeb030bffd2b9d45c50eda9781
TauCeti0207 <2076188013@qq.com>
2024-09-20 14:28:44 +0800

    Auto commit for 2024-09-20

---
+++ build/vision_msgs/CMakeFiles/vision_msgs_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_BoundingBox2D.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_BoundingBox3D.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_Classification2D.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_Classification3D.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_Detection2D.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_Detection2DArray.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_Detection3D.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_Detection3DArray.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_ObjectHypothesis.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_ObjectHypothesisWithPose.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/_VisionInfo.py"
+  "/home/yzq0207/catkin_ws/devel/lib/python3/dist-packages/vision_msgs/msg/__init__.py"
+  "CMakeFiles/vision_msgs_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vision_msgs_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/gennodejs/ros/vision_msgs/msg/_index.js
@@ -0,0 +1,28 @@
+
+"use strict";
+
+let ObjectHypothesisWithPose = require('./ObjectHypothesisWithPose.js');
+let Classification3D = require('./Classification3D.js');
+let Detection2DArray = require('./Detection2DArray.js');
+let Classification2D = require('./Classification2D.js');
+let BoundingBox3D = require('./BoundingBox3D.js');
+let ObjectHypothesis = require('./ObjectHypothesis.js');
+let Detection2D = require('./Detection2D.js');
+let Detection3DArray = require('./Detection3DArray.js');
+let Detection3D = require('./Detection3D.js');
+let VisionInfo = require('./VisionInfo.js');
+let BoundingBox2D = require('./BoundingBox2D.js');
+
+module.exports = {
+  ObjectHypothesisWithPose: ObjectHypothesisWithPose,
+  Classification3D: Classification3D,
+  Detection2DArray: Detection2DArray,
+  Classification2D: Classification2D,
+  BoundingBox3D: BoundingBox3D,
+  ObjectHypothesis: ObjectHypothesis,
+  Detection2D: Detection2D,
+  Detection3DArray: Detection3DArray,
+  Detection3D: Detection3D,
+  VisionInfo: VisionInfo,
+  BoundingBox2D: BoundingBox2D,
+};
